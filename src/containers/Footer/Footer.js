7e4d6635456c805ff33bf626933466b0e50feef0
Tiago Cintra <tiagosilvacintra@yahoo.com.br>
2019-10-20 21:53:06 -0300

    Usuário e mensagem

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { enabled } from 'ansi-colors';
 
-const StyledDiv = styled.div`
+const FooterDiv = styled.div`
   position: fixed;
   left: 0;
   bottom: 0;
@@ -12,6 +13,24 @@ const StyledDiv = styled.div`
   border-top: 2px solid #000;
   padding: 10px;
   font-family: sans-serif, Arial;
+`;
+
+const UserDiv = styled.div`
+  float: left;
+  margin: 0;
+  width: 20%;
+`;
+
+const MessageDiv = styled.div`
+  float: left;
+  margin: 0;
+  width: 70%;
+`;
+
+const SendDiv = styled.div`
+  float: left;
+  margin: 0;
+  width: 10%;
 `;
 
 const Input = styled.input`
@@ -56,13 +75,78 @@ const Button = styled.button`
   }
 `;
 
-const footer = props => {
+const TextArea = styled.textarea`
+  outline: none;
+  font-family: inherit;
+  box-sizing: border-box;
+  border: 1px solid #000;
+  border-radius: 10px;
+  box-shadow: 2px 2px 2px #966909;
+  padding: 3px 5px;
+  width: 100%;
+  height: 50px;
+  resize: none;
+`;
+
+const Footer = props => {
+  const [userName, setUserName] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [inputEnable, setInputEnable] = useState(true);
+  const [userButtonText, setUserButtonText] = useState('Confirmar');
+  const [textAreaValue, setTextAreaValue] = useState('');
+  const [messageEnable, setMessageEnable] = useState(false);
+
+  const inputChangedHandler = event => {
+    setInputValue(event.target.value);
+  };
+
+  const userNameHandler = () => {
+    if (inputValue === '') {
+      alert('Digite no nome do usuário.');
+      return;
+    }
+
+    setUserName(inputValue);
+
+    if (userButtonText === 'Confirmar') {
+      setInputEnable(false);
+      setMessageEnable(true);
+      setUserButtonText('Cancelar');
+    } else {
+      setInputEnable(true);
+      setMessageEnable(false);
+      setUserName('');
+      setInputValue('');
+      setTextAreaValue('');
+      setUserButtonText('Confirmar');
+    }
+  };
+
+  const textAreaChangedHandler = event => {
+    setTextAreaValue(event.target.value);
+  };
+
   return (
-    <StyledDiv>
-      <Input placeholder="Nome do usuário" />
-      <Button>Confirmar</Button>
-    </StyledDiv>
+    <FooterDiv>
+      <UserDiv>
+        <Input
+          placeholder="Nome do usuário"
+          value={inputValue}
+          onChange={event => inputChangedHandler(event)}
+          disabled={!inputEnable}
+        />
+        <Button onClick={userNameHandler}>{userButtonText}</Button>
+      </UserDiv>
+      <MessageDiv>
+        <TextArea value={textAreaValue} onChange={textAreaChangedHandler} disabled={!messageEnable}>
+          {props.message}
+        </TextArea>
+      </MessageDiv>
+      <SendDiv>
+        <Button disabled={!messageEnable}>Enviar</Button>
+      </SendDiv>
+    </FooterDiv>
   );
 };
 
-export default footer;
+export default Footer;
